67a211d255d0d6c4f3a6c2f4842ac83ae0ed7d17
orestesoliveira <orestesoliveira@gmail.com>
2024-04-17 12:11:49 -0300

    Initial version with gradle dependencies.

---
+++ src/main/java/com/bechallenge/orestesoliveirajavabe/OrestesOliveiraJavaBeApplication.java
@@ -0,0 +1,13 @@
+package com.bechallenge.orestesoliveirajavabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrestesOliveiraJavaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrestesOliveiraJavaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bechallenge/orestesoliveirajavabe/OrestesOliveiraJavaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bechallenge.orestesoliveirajavabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrestesOliveiraJavaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
